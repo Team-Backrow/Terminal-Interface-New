bc07fe1f0da59457c68c5fa9b575066438ee5d5b
Josh Kilgore <kilgore.josh@gmail.com>
2017-04-04 19:31:36 -0500

    initial setup SQL folder and script created

---
+++ Bangazon-Terminal-App/Bangazon-Terminal-App.consoleapp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bangazon_Terminal_App.consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
