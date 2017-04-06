c1057cde315f54034ea547e15e38411bac887b16
Andi DeVois <adevois@gmail.com>
2017-04-06 18:01:53 -0500

    Added methods to IRepository to use with the databse

--- Bangazon-Terminal-App/Bangazon-Terminal-App.consoleapp/DAL/IRepository.cs
+++ Bangazon-Terminal-App/Bangazon-Terminal-App.consoleapp/DAL/IRepository.cs
@@ -7,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace Bangazon_Terminal_App.consoleapp.DAL
 {
-    interface IRepository
-    { 
+    public interface IRepository
+    {
+        void AddCustomer(int CustomerID);
+        void GetCustomer(int CustomerID);
+        void AddProduct(int ProductID);
+        void GetProduct(int ProductID);
+        void GetOrder(int OrderID);
+        void RemoveProduct(int ProductID, int OrderID);
+        void AddPayment(int PaymentTypeID);
+        void GetPayment(int PaymentTypeID);
     }
 }
