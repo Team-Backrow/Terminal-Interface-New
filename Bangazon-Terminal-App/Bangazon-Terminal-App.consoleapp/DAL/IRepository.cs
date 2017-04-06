2f0d0999c4d1a5990ac2a53bdbb13fa3c1751afb
Andi DeVois <adevois@gmail.com>
2017-04-06 17:56:25 -0500

    scaffolding repositories

---
+++ Bangazon-Terminal-App/Bangazon-Terminal-App.consoleapp/DAL/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Bangazon_Terminal_App.consoleapp.DAL
+{
+    interface IRepository
+    { 
+    }
+}
