8e6092e23a842d58ad3af8e721e67374657b52aa
Andi DeVois <adevois@gmail.com>
2017-04-08 19:04:45 -0500

    added 10 products to database

---
+++ SQL/Insert_Products_4_8_17.sql
@@ -0,0 +1,30 @@
+USE [TerminalInterface]
+GO
+
+INSERT INTO [dbo].[Product]
+           ([ProductName]
+           ,[ProductPrice])
+     VALUES
+			('Instant sushi'
+           ,15.98),
+		              ('Unicorn onesie'
+           ,25.36),
+		              ('Dinosaur skeleton set'
+           ,8.02),
+		              ('Cat translator'
+           ,99.55),
+		              ('Dog treats'
+           ,3.99),
+		              ('Armchair covers'
+           ,30.00),
+		              ('Assorted batteries'
+           ,19.19),
+		              ('Scratched mirror'
+           ,2.00),
+		              ('Mismatched plates'
+           ,7.37),
+		              ('Gardening set'
+           ,13.99)
+GO
+
+
